459978354cd0b703537ecb47130459265f60f666
Rishabh <rishabh.adstacks@gmail.com>
2020-09-26 08:38:43 +0530

    Added Transaction entity

---
+++ src/main/java/in/novopay/wallet/beans/Transaction.java
@@ -0,0 +1,37 @@
+package in.novopay.wallet.beans;
+
+import in.novopay.wallet.enums.TransactionType;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
+public class Transaction {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    @Column(length = 50)
+    String id;
+
+    @Column(nullable = false)
+    TransactionType type;
+
+    @ManyToOne
+    @Column(nullable = false)
+    User primary;  //main entity
+
+    @ManyToOne
+    User secondary; //only applicable in case of transfers
+
+    @CreationTimestamp
+    Date creationTimestamp;
+
+    @UpdateTimestamp
+    Date updationTimestamp;
+}
