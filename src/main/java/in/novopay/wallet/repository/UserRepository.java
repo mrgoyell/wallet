b163288bdb88047e0a91806d99ac4e7f824dd832
Rishabh <rishabh.adstacks@gmail.com>
2020-09-26 08:07:01 +0530

    Added User Repo

---
+++ src/main/java/in/novopay/wallet/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package in.novopay.wallet.repository;
+
+import in.novopay.wallet.beans.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface UserRepository extends JpaRepository<User,Long> {
+}
