d0a8ebcefcae72e75daf1b7820b1a27a5f33fef7
Rishabh <rishabh.adstacks@gmail.com>
2020-09-26 07:57:55 +0530

    Added validator and init User

--- src/main/java/in/novopay/wallet/beans/User.java
+++ src/main/java/in/novopay/wallet/beans/User.java
@@ -1,6 +1,30 @@
 package in.novopay.wallet.beans;
 
-@Entity
-public class User{
+import lombok.Data;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String name;
+
+    @NotEmpty
+    @Size(min = 8, max = 16)
+    @Column(unique = true, length = 16)
+    String phone;
+
+    @NotEmpty
+    @Size(min = 3, max = 240)
+    @Column(unique = true, length = 240)
+    String email;
+
+    String password;
+    float wallet;
 }
